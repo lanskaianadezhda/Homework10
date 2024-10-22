714783b4441d70874f279b8c59edfcc65f69d574
lanskaianadezhda <nadin-91@bk.ru>
2024-10-22 17:00:15 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+
         //task 1
         System.out.println("Задача 1");
+
+        String firstName = "Иван";
+        String midleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + midleName;
+        System.out.println(fullName);
+
+        //task
+        System.out.println("Задача 2");
+
+        fullName = fullName.toUpperCase();
+        System.out.println(fullName);
+
+        //task 3
+        System.out.println("Задача 3");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println(fullName);
     }
 }
